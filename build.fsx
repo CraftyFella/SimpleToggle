3c86a294d6b65793abb1339633fd315d35c6362d
Dave Craft <CraftyFella@gmail.com>
2015-03-27 14:28:33 +0000

    FAKE runs tests and creates packages using Paket

--- build.fsx
+++ build.fsx
@@ -1,35 +1,37 @@
 ﻿// include Fake lib
 #r "packages/FAKE/tools/FakeLib.dll"
+
 open Fake
 
 // Properties
 let solutionFile = "SimpleToggle.sln"
 let testDir = "./test/"
+let packageDir = "./package/"
 let testDlls = !!"/**/bin/Release/*Tests.dll"
+let xUnitPath = "packages/xunit.runner.console/tools/xunit.console.exe"
 
-Target "Build" (fun _ ->
-    !! solutionFile
-      |> MSBuildRelease "" "Build"
-      |> Log "AppBuild-Output: "
-)
+let buildNumber = 
+    match appVeyorBuildVersion with
+    | null -> "0.0.2"
+    | _ -> appVeyorBuildVersion
 
-Target "Test" (fun _ ->
-    testDlls
-      |> xUnit (fun p ->
-          {p with
-             ToolPath = "packages/xunit.runner.console/tools/xunit.console.exe";
-             OutputDir = testDir;
-              })
-)
+Target "Build" (fun _ -> 
+    !!solutionFile
+    |> MSBuildRelease "" "Build"
+    |> Log "AppBuild-Output: ")
+Target "Test" (fun _ -> 
+    testDlls |> xUnit (fun p -> 
+                    { p with ToolPath = xUnitPath
+                             OutputDir = testDir }))
 
-Target "Default" (fun _ ->
-    trace "Hello World from FAKE"
-)
-
+Target "NuGet" (fun _ -> 
+    Paket.Pack(fun p -> 
+        { p with Version = buildNumber
+                 OutputPath = packageDir }))
+Target "Default" DoNothing
+Target "CI" DoNothing
 // Dependencies
-"Build"
-  ==> "Test"
-  ==> "Default"
-
+"Build" ==> "Test" ==> "Default"
+"Build" ==> "Test" ==> "NuGet" ==> "CI"
 // start build
 RunTargetOrDefault "Default"
