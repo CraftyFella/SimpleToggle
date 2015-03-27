e838ce70c5bacf90a71e9a034f4e16081f5a6954
Dave Craft <CraftyFella@gmail.com>
2015-03-27 15:53:51 +0000

    Publish package to nuget feed

--- build.fsx
+++ build.fsx
@@ -9,10 +9,12 @@ let testDir = "./test/"
 let packageDir = "./package/"
 let testDlls = !!"/**/bin/Release/*Tests.dll"
 let xUnitPath = "packages/xunit.runner.console/tools/xunit.console.exe"
+let nugetKey = environVar "NUGETKEY"
+let nugetUrl = environVar "NUGETURL"
 
 let buildNumber = 
     match appVeyorBuildVersion with
-    | null -> "0.0.2"
+    | null -> "0.0.0"
     | _ -> appVeyorBuildVersion
 
 Target "Build" (fun _ -> 
@@ -23,11 +25,14 @@ Target "Test" (fun _ ->
     testDlls |> xUnit (fun p -> 
                     { p with ToolPath = xUnitPath
                              OutputDir = testDir }))
-
 Target "NuGet" (fun _ -> 
     Paket.Pack(fun p -> 
         { p with Version = buildNumber
-                 OutputPath = packageDir }))
+                 OutputPath = packageDir })
+    Paket.Push(fun p -> 
+        { p with ApiKey = nugetKey
+                 PublishUrl = nugetUrl
+                 WorkingDir = packageDir }))
 Target "Default" DoNothing
 Target "CI" DoNothing
 // Dependencies
