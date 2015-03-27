458c085b87c201de56e263dbb3f0d90b27a38c54
Dave Craft <CraftyFella@gmail.com>
2015-03-27 10:32:35 +0000

    Adding FAKE

---
+++ build.fsx
@@ -0,0 +1,35 @@
+﻿// include Fake lib
+#r "packages/FAKE/tools/FakeLib.dll"
+open Fake
+
+// Properties
+let solutionFile = "SimpleToggle.sln"
+let testDir = "./test/"
+let testDlls = !!"/**/bin/Release/*Tests.dll"
+
+Target "Build" (fun _ ->
+    !! solutionFile
+      |> MSBuildRelease "" "Build"
+      |> Log "AppBuild-Output: "
+)
+
+Target "Test" (fun _ ->
+    testDlls
+      |> xUnit (fun p ->
+          {p with
+             ToolPath = "packages/xunit.runner.console/tools/xunit.console.exe";
+             OutputDir = testDir;
+              })
+)
+
+Target "Default" (fun _ ->
+    trace "Hello World from FAKE"
+)
+
+// Dependencies
+"Build"
+  ==> "Test"
+  ==> "Default"
+
+// start build
+RunTargetOrDefault "Default"
